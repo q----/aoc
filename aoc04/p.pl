167ea5cd6526b0839888dfdee608152441377c2d
q <root@gitlab.local>
2021-07-23 09:52:59 -0500

    simplify ranges

--- aoc04/p.pl
+++ aoc04/p.pl
@@ -16,8 +16,8 @@ sub Valid{
   return '' if not $t =~ /(byr:((19[^01]\d)|(200[012])))/;
   return '' if not $t =~ /(iyr:20((1\d)|(20)))/;
   return '' if not $t =~ /(eyr:20((2\d)|(30)))/;
-  return '' if not $t =~ /(hgt:((1(([5678]\d)|(9[0123]))cm)|(((59)|(6\d)|(7[0123456]))in)))/;
-  return '' if not $t =~ /(hcl:#[0123456789abcdef]{6} )/;
+  return '' if not $t =~ /(hgt:((1(([5-8]\d)|(9[0-3]))cm)|(((59)|(6\d)|(7[0-6]))in)))/;
+  return '' if not $t =~ /(hcl:#[0-9a-f]{6} )/;
   return '' if not $t =~ /(ecl:((amb)|(blu)|(brn)|(gry)|(grn)|(hzl)|(oth)))/;
   return '' if not $t =~ /(pid:\d{9} )/;
   return 1;
